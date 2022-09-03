628740d7381c74fe1aac5655af51134ab3f3d415
Andres Rivero <andres19rivero@gmail.com>
2022-09-03 17:39:18 -0400

    trigger the ngOnChanges hook with and Input

--- src/app/components/sample-name/sample-name.component.ts
+++ src/app/components/sample-name/sample-name.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-sample-name',
   templateUrl: './sample-name.component.html',
   styleUrls: ['./sample-name.component.css']
 })
-export class SampleNameComponent implements OnInit {
+export class SampleNameComponent implements OnInit, OnChanges {
+  @Input() name!: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+  }
 }
